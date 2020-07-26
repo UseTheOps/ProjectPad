3d6660afe021a6152d5ec05c299fa29074c79c2b
mcarbenay <michael@carbenay.info>
2020-07-26 08:25:01 +0200

    Testing UWP Commanding (2)

---
+++ ProjectPad.Windows/ProjectPad.Business/CommandBases.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ProjectPad.Business
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        protected void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+    }
+
+    public abstract class ProjectCommandBase : CommandBase
+    {
+        protected ProjectViewModel _project;
+        public ProjectCommandBase(ProjectViewModel project)
+        {
+            _project = project;
+        }
+
+    }
+
+}
---
+++ ProjectPad.Windows/ProjectPad.Business/ProjectViewModel - Commands.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProjectPad.Business
+{
+    partial class ProjectViewModel
+    {
+
+
+        public class AddContentCommand : ProjectCommandBase
+        {
+            public AddContentCommand(ProjectViewModel project) : base(project)
+            {
+            }
+
+            public override bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public override void Execute(object parameter)
+            {
+                var kind = ProjectViewModel.ProjectItemKind.TextContent;
+                switch (parameter?.ToString()?.ToLowerInvariant())
+                {
+                    case "title":
+                        kind = ProjectViewModel.ProjectItemKind.Title;
+                        break;
+                }
+
+                _project.AddItem(kind);
+            }
+        }
+    }
+}
